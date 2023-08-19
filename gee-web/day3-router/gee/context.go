358bbc9b185a86b67b3c21a22e58fa1d1d14cac0
lxhcaicai <2778763221@qq.com>
2023-08-19 11:52:33 +0800

    add day3-router 单元测试

---
+++ gee-web/day3-router/gee/context.go
@@ -0,0 +1,40 @@
+package gee
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type H map[string]interface{}
+
+type Context struct {
+	// origin objects
+	Writer http.ResponseWriter
+	Req    *http.Request
+	// request info
+	Path   string
+	Method string
+	Params map[string]string
+	// response info
+	StatusCode int
+}
+
+func (c *Context) Param(key string) string {
+	value, _ := c.Params[key]
+	return value
+}
+
+func (c *Context) SetHeader(key string, value string) {
+	c.Writer.Header().Set(key, value)
+}
+
+func (c *Context) Status(code int) {
+	c.StatusCode = code
+	c.Writer.WriteHeader(code)
+}
+
+func (c *Context) String(code int, format string, values ...interface{}) {
+	c.SetHeader("Content-Type", "text/plain")
+	c.Status(code)
+	c.Writer.Write([]byte(fmt.Sprintf(format, values...)))
+}
---
+++ gee-web/day3-router/gee/gee.go
@@ -0,0 +1,3 @@
+package gee
+
+type HandleFunc func(*Context)
