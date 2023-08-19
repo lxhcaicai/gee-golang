5fc4f5376857334c8905ce63cbd419ac2035e291
lxhcaicai <2778763221@qq.com>
2023-08-20 00:44:34 +0800

    add day4-consistent-hash geecache init

---
+++ gee-cache/day4-consistent-hash/geecache/http.go
@@ -0,0 +1,60 @@
+package geecache
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+const defaultBasePath = "/_geecache/"
+
+type HTTPPool struct {
+	// this peer's base URL, e.g. "https://example.net:8000"
+	self     string
+	basePath string
+}
+
+// NewHTTPPool初始化对等体的HTTP池。
+func NewHTTPPool(self string) *HTTPPool {
+	return &HTTPPool{
+		self:     self,
+		basePath: defaultBasePath,
+	}
+}
+
+// 带有服务器名称的日志信息
+func (p *HTTPPool) Log(format string, v ...interface{}) {
+	log.Println("[Server %s] %s", p.self, fmt.Sprintf(format, v...))
+}
+
+// ServeHTTP handle all http requests
+func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if !strings.HasPrefix(r.URL.Path, p.basePath) {
+		panic("HTTPPool serving unexpected path: " + r.URL.Path)
+	}
+	p.Log("%s %s", r.Method, r.URL.Path)
+
+	parts := strings.SplitN(r.URL.Path[len(p.basePath):], "/", 2)
+	if len(parts) != 2 {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+
+	groupName := parts[0]
+	key := parts[1]
+
+	group := GetGroup(groupName)
+	if group == nil {
+		http.Error(w, "no such group: "+groupName, http.StatusNotFound)
+	}
+
+	view, err := group.Get(key)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/octet-stream")
+	w.Write(view.ByteSlice())
+}
